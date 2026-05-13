6af9f879dd304d352b73906b3e1780ee4aa94a8f
sakicansev <sakicansev@gmail.com>
2026-05-13 19:00:26 +0200

    feature: add price classification with if/else

--- practice.js
+++ practice.js
@@ -13,8 +13,17 @@ const coins =[
 
 function printCoin(coin) {
         console.log(coin.name + " (" + coin.symbol +"): $" + coin.price.toFixed(2));
+        if (coin.price < 1) {
+            console.log("  ⚠ under $1");
+        } else if (coin.price > 10000) {
+            console.log( "  * major coin");
+        } else {
+            console.log("  . mid range");
+            }
+            
+        }
 
-}
+
 
 for (let i = 0; i < coins.length; i++) {
     printCoin(coins[i])
